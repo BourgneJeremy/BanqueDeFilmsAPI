29032b338f5085d26631cd64ac29b37639505ae9
BourgneJeremy <jeremy.bourgne@ynov.com>
2022-10-15 13:52:20 +0200

    Ajoutez des fichiers projet.

---
+++ BanqueDeFilmsAPI/APIMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanqueDeFilmsAPI
+{
+    internal class APIMethods
+    {
+        public void GetMovies()
+        {
+
+        }
+    }
+}
